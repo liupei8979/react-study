002115ecf84f7553c331c39ec133fb717e2f4c6b
liupei8979 <khsr98@ajou.ac.kr>
2023-06-01 20:57:41 +0900

    Home 화면

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+/*
+
+import React from "react";
+
+
+function Home(){
+    retrun <h1>Home 화면 입니다.</h1>
+}
+*/
+
+import React, {Component} from "react";
+
+class Home extends Component{
+    render(){
+        return <h1>Home 화면 입니다.</h1>
+    }
+}
+
+export default Home;
