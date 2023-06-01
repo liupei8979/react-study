1e7f0a5821df45d885f0aff3528628983e4d9185
liupei8979 <khsr98@ajou.ac.kr>
2023-06-01 21:12:31 +0900

    route 연습

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import {Routes, Route,Link} from "react-router-dom";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+
+      </nav>
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,10 @@
-/*
+
 
 import React from "react";
 
 
-function Home(){
-    retrun <h1>Home 화면 입니다.</h1>
-}
-*/
-
-import React, {Component} from "react";
-
-class Home extends Component{
-    render(){
-        return <h1>Home 화면 입니다.</h1>
-    }
+const Home =() => {
+   return <h1>Home 화면 입니다.</h1>
 }
 
 export default Home;
