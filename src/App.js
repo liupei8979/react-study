4b982117360df1eacbaf7978116d824eba24d621
liupei8979 <khsr98@ajou.ac.kr>
2023-06-02 21:21:34 +0900

    Counter 증가 감소 추가 연습

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import {Routes, Route,Link} from "react-router-dom";
-
+import Counter from "./pages/Counter";
 import Home from "./pages/Home";
 import About from "./pages/About";
 function App() {
   return (
     <div className="App">
       <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link>
 
       </nav>
       <Routes>
         <Route path = "/" element={<Home/>}/>
         <Route path = "/about" element={<About/>}/>
+        <Route path="/counter" elemnet={<Counter />} />
       </Routes>
     </div>
   );
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
  const About =() => {
-    return <h1>Home 화면 입니다.</h1>
+    return <h1>About 화면 입니다.</h1>
 }
 
 export default About;
